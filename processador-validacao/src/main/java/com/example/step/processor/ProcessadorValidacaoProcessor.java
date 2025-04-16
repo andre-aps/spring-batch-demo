95e5939c95df6f1f921fffd649003495889776ac
André <andre.psilva@picpay.com>
2025-04-15 22:39:32 -0300

    adicionando processador composto

--- processador-validacao/src/main/java/com/example/step/processor/ProcessadorValidacaoProcessor.java
+++ processador-validacao/src/main/java/com/example/step/processor/ProcessadorValidacaoProcessor.java
@@ -2,7 +2,9 @@ package com.example.step.processor;
 
 import com.example.domain.Cliente;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.support.builder.CompositeItemProcessorBuilder;
 import org.springframework.batch.item.validator.BeanValidatingItemProcessor;
+import org.springframework.batch.item.validator.ValidatingItemProcessor;
 import org.springframework.batch.item.validator.ValidationException;
 import org.springframework.batch.item.validator.Validator;
 import org.springframework.context.annotation.Bean;
@@ -17,17 +19,24 @@ public class ProcessadorValidacaoProcessor {
     private Set<String> emails = new HashSet<>();
 
     @Bean
-    public ItemProcessor<Cliente, Cliente> processadorValidacaoItemProcessor() {
-        //        Validação utilizando BeanValidatingItemProcessor
+    public ItemProcessor<Cliente, Cliente> processadorValidacaoItemProcessor() throws Exception {
+        return new CompositeItemProcessorBuilder<Cliente, Cliente>()
+            .delegates(beanValidatingItemProcessor(), emailValidatingProcessor())
+            .build();
+    }
+
+    private BeanValidatingItemProcessor<Cliente> beanValidatingItemProcessor() throws Exception {
         BeanValidatingItemProcessor<Cliente> processor = new BeanValidatingItemProcessor<>();
         processor.setFilter(true);
+        processor.afterPropertiesSet();
         return processor;
+    }
 
-        //        Validação customizada
-        //        ValidatingItemProcessor<Cliente> processor = new ValidatingItemProcessor<>();
-        //        processor.setValidator(validator());
-        //        processor.setFilter(true);
-        //        return processor;
+    private ValidatingItemProcessor<Cliente> emailValidatingProcessor() {
+        ValidatingItemProcessor<Cliente> processor = new ValidatingItemProcessor<>();
+        processor.setValidator(validator());
+        processor.setFilter(true);
+        return processor;
     }
 
     private Validator<? super Cliente> validator() {
