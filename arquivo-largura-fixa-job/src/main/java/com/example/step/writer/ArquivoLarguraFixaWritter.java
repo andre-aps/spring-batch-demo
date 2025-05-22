aaad9d16401b839b58a57a06a670da3b01e9a4c3
André <andre.psilva@picpay.com>
2025-05-21 21:53:59 -0300

    adicionando escrita em arquivo flat formatado

--- arquivo-largura-fixa-job/src/main/java/com/example/step/writer/ArquivoLarguraFixaWritter.java
+++ arquivo-largura-fixa-job/src/main/java/com/example/step/writer/ArquivoLarguraFixaWritter.java
@@ -1,16 +1,28 @@
 package com.example.step.writer;
 
 import com.example.domain.Cliente;
-import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.WritableResource;
 
 @Configuration
 public class ArquivoLarguraFixaWritter {
 
+    @StepScope
     @Bean
-    public ItemWriter<Cliente> arquivoLarguraFixaItemWriter() {
-        return items -> items.forEach(System.out::println);
+    public FlatFileItemWriter<Cliente> arquivoLarguraFixaItemWriter(
+        @Value("#{jobParameters['arquivoClientesSaida']}") WritableResource arquivoClientesSaida) {
+        return new FlatFileItemWriterBuilder<Cliente>()
+            .name("arquivoLarguraFixaItemWriter")
+            .resource(arquivoClientesSaida)
+            .formatted()
+            .format("%-9s %-9s %-2s %-19s")
+            .names("nome", "sobrenome", "idade", "email")
+            .build();
     }
 
 }
