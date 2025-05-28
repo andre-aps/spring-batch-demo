e703160d22eef24d2c8b5e14503aab5071a3b874
André <andre.psilva@picpay.com>
2025-05-28 20:34:10 -0300

    adicionando escrita em arquivo flat delimitado

--- arquivo-delimitado-job/src/main/java/com/example/step/writer/ArquivoDelimitadoWritter.java
+++ arquivo-delimitado-job/src/main/java/com/example/step/writer/ArquivoDelimitadoWritter.java
@@ -1,16 +1,27 @@
 package com.example.step.writer;
 
 import com.example.domain.Cliente;
-import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.WritableResource;
 
 @Configuration
 public class ArquivoDelimitadoWritter {
 
+    @StepScope
     @Bean
-    public ItemWriter<Cliente> arquivoDelimitadoItemWriter() {
-        return items -> items.forEach(System.out::println);
+    public FlatFileItemWriter<Cliente> arquivoDelimitadoItemWriter(
+        @Value("#{jobParameters['arquivoClientesSaida']}") WritableResource arquivoClientesSaida) {
+        return new FlatFileItemWriterBuilder<Cliente>()
+            .name("arquivoDelimitadoItemWriter")
+            .resource(arquivoClientesSaida)
+            .delimited()
+            .names("nome", "sobrenome", "idade", "email")
+            .build();
     }
 
 }
